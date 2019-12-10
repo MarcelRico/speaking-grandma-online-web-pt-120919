4dfe3dd33b5cb2ede7f25f6667b575931a8a619c
Marcel Rico <personal.mr4@gmail.com>
2019-12-10 02:46:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,6 +14,10 @@
 
 
 def speak_to_grandma(phrase)
-  if(phrase.upcase == phrase)
-    return ""
+  if(phrase == "I LOVE YOU GRANDMA!")
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif(phrase.upcase == phrase)
+    return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
 end
